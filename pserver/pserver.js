4cc2044d9096f7a5b41647c32c5b575e66ee8ed0
John Unterholzner <unterhol@gmail.com>
2021-01-07 16:27:17 -0600

    more updates

--- pserver/pserver.js
+++ pserver/pserver.js
@@ -3,23 +3,29 @@ var express = require('express');
 
 var app = express();
 
+var inputs = [{ pin: '11', gpio: '17', value: 1 },
+{ pin: '12', gpio: '18', value: 0 }];
+
 app.use(express['static'](__dirname));
 
-// Express route
-app.get('inputs/:id', function(req, res) {
-    res.status(200).send("hello")
+// Express route for incoming requests for a customer name
+app.get('/inputs/:id', function(req, res) {
+    res.status(200).send(inputs[req.params.id]);
 });
-
-// Express route unknown handler
+    
+// Express route for any other unrecognised incoming requests
 app.get('*', function(req, res) {
-    res.status(404).send("Oops, route not found")
+    res.status(404).send('Unrecognised API call');
+});
+    
+// Express route to handle errors
+app.use(function(err, req, res, next) {
+    if (req.xhr) {
+    res.status(500).send('Oops, Something went wrong!');
+    } else {
+    next(err);
+    }
 });
 
-app.use(function(err, req, res, next){
-    if (req.xhr) {
-        res.status(500).send('Oops, 500 error)');
-    }
-    else {
-        next(err);
-    }
-});
+app.listen(3000);
+console.log('App Server running at port 3000')
